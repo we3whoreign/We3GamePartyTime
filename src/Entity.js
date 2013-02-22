5b27bca46bcf995bb30667e7565185287450ea70
ryzngard <ryzngard@gmail.com>
2013-02-21 22:00:42 -0800

    Added entity.js
    Started thinking about how to describe objects in space and handle
    collisions. Using polygons as a bounding shape allows for linear algebra
    calculations using the bounding lines and point triangulation as a
    backup.

---
+++ src/Entity.js
@@ -0,0 +1,34 @@
+Entity = new function (shape) {
+	//boundingShape is the prototype of some polygon
+	this.shape = shape;
+};
+
+Entity.prototype.center = {
+	x: 0,
+	y: 0;
+};
+
+Entity.prototype.octogonalDirections = {
+	NORTH: 0,
+	SOUTH: 0,
+	EAST: 0,
+	WEST: 0,
+	NE: 0,
+	SE: 0,
+	SW: 0,
+	NW: 0;
+
+};
+
+
+Entity.prototype.getBoundingShape = function () {
+	if(!this._boundingShape){
+		this._boundingShape = new this._boundingShape(this);
+	}
+	
+	return this.boundingShape;
+};
+
+
+Entity.prototype._boundingShape = null;
+Entity.prototype.visible = false;
