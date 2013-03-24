eb2bee824ccc524477fbb4a1095f600a1377c15a
ryzngard <ryzngard@gmail.com>
2013-03-23 18:07:22 -0700

    moved boundingrectangle to it's own file

--- src/games/Pong.js
+++ src/games/Pong.js
@@ -4,43 +4,6 @@
 	canvas.height = 600;
 	var	context = canvas.getContext('2d');
 	
-	function BoundingRectangle (height, width, centerPoint) {
-		this.center = centerPoint;
-		this.height = height;
-		this.width = width;
-		//Define just a basic rectangle, evently spaced about the center point
-		
-		var x,
-			y;
-			
-		// Lower left corner
-		x = centerPoint.x - 0.5*width;
-		y = centerPoint.y - 0.5*height;
-		var lowerLeft = new Point(x,y);
-		
-		// Lower right corner
-		x += width;
-		var lowerRight = new Point(x,y);
-		
-		// Upper right corner
-		y += height;
-		var upperRight = new Point(x,y);
-		
-		// Upper left corner
-		x -= width;
-		var upperLeft = new Point(x,y);
-		
-		this.addLine(new Line(upperRight, upperLeft));
-		this.addLine(new Line(upperRight, lowerRight));
-		this.addLine(new Line(lowerRight, lowerLeft));
-		this.addLine(new Line(upperLeft, lowerLeft));
-	};
-	
-	BoundingRectangle.prototype.width = 0;
-	BoundingRectangle.prototype.height = 0;
-	
-	BoundingRectangle.prototype = new BoundingShape();
-	
 	var leftPaddle = new Entity(),
 		rightPaddle = new Entity();
 	
